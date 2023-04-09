e1ae352ba2ba7786496feecd67ed044dca03a76a
k_takahashi <k_takahashi@just-ltd.co.jp>
2023-04-09 16:12:40 +0900

    chap5

---
+++ chap05/chap05/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const inputNameBind = ref("しんちゃ");
+const inputNameOn = ref("ななし");
+const onInputName = (event: Event): void => {
+  const element = event.target as HTMLInputElement;
+  inputNameBind.value = element.value;
+}
+</script>
+
+<template>
+  <section>
+    <input type="text" v-model="inputNameBind">
+    <p>{{ inputNameBind }}</p>
+  </section>
+  <br>
+  <!-- <section>
+    <input type="text" v-on:input = "onInputName">
+    <p>{{ inputNameOn }}</p>
+  </section> -->
+</template>
