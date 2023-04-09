4203110d015ee1ba5bfcf422025f2f78304bdd93
k_takahashi <k_takahashi@just-ltd.co.jp>
2023-04-09 16:46:38 +0900

    object-literal

--- chap05/chap05/directive-loop-map/src/App.vue
+++ chap05/chap05/directive-loop-map/src/App.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-const cooktailListInit = new Map<number, string>();
-cooktailListInit.set(2345, 'white lady');
-cooktailListInit.set(6789, 'blue hawai');
-cooktailListInit.set(1234, 'new york');
-const cooktailList = ref(cooktailListInit)
+const whiteLadyInit: {
+  id: number
+  name: string
+  price: number
+  recipe: string
+} = {
+  id: 1234,
+  name: 'white lady',
+  price: 1200,
+  recipe: 'ほげふが'
+}
+const whiteLady = ref(whiteLadyInit)
 </script>
 
 <template>
-  <ul>
-    <li v-for="[id, cookTailName] in cooktailList"
-        v-bind:key="id">
-        IDが{{ id }}のカクテルは{{ cookTailName }}
-    </li>
-  </ul>
+  <dt>
+    <template v-for="(val, key) in whiteLady" v-bind:key="key">
+      <dt>{{ key }}</dt>
+      <dd>{{ val }}</dd>
+    </template>
+  </dt>
 </template>
